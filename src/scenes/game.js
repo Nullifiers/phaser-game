5272bd1b852a6932767e37c7d356dc2a32250877
Lakshya Gupta <theultimateaim@gmail.com>
2019-11-27 10:24:01 +0530

    More levels

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -12,14 +12,30 @@ class Game extends Phaser.Scene {
     preload() {
     }
 
-    createPlayer() {
-        this.player = this.add.rectangle(100, 550, config.playerWidth, config.playerHeight, 0xffffff);
-        this.player.setOrigin(0, 0);
-        this.physics.add.existing(this.player);
-        this.player.body.setCollideWorldBounds(true);
+    createBoundaries() {
+        const bound1 = this.add.rectangle(0, -config.height * 10, 1, config.height * 11, 0x000000);
+        bound1.setOrigin(0, 0);
+        this.physics.add.existing(bound1);
+        bound1.body.setImmovable(true);
+        bound1.body.allowGravity = false;
+        this.physics.add.collider(this.player, bound1);
+
+        const bound2 = this.add.rectangle(config.width - 1, -config.height * 10, 1, config.height * 11, 0x000000);
+        bound2.setOrigin(0, 0);
+        this.physics.add.existing(bound2);
+        bound2.body.setImmovable(true);
+        bound2.body.allowGravity = false;
+        this.physics.add.collider(this.player, bound2);
     }
 
-    createPlatform(x, y, width, height) {
+    createPlayer() {
+        this.player = this.add.rectangle(100, -100, config.playerWidth, config.playerHeight, 0xffffff);
+        this.player.setOrigin(0, 0);
+        this.physics.add.existing(this.player);
+        // this.player.body.setCollideWorldBounds(true);
+    }
+
+    createPlatform(x, y, width, height = config.platformHeight) {
         const platform = this.add.rectangle(x, y, width, height, 0x333333);
         platform.setOrigin(0, 0);
         this.physics.add.existing(platform);
@@ -29,15 +45,26 @@ class Game extends Phaser.Scene {
     }
 
     createPlatforms() {
-        this.createPlatform(0, config.height - config.platformHeight - 100, config.width, config.platformHeight);
-        this.createPlatform(0, config.height - config.platformHeight, config.width - 16, config.platformHeight);
-        this.createPlatform(config.width - 16, config.height - 0.1, 16, config.platformHeight);
-        this.createPlatform(0, config.height - config.platformHeight * 2 - config.playerHeight - 100, config.width, config.platformHeight);
+        const platforms = [
+            [0, config.height - config.platformHeight, config.width - 16],
+            [config.width - 16, config.height - 0.1, 16],
+            [0, config.height - 100, config.width],
+            [0, config.height - 100 - 32, config.width],
+            [500, config.height - 190, 100],
+            [600, config.height - 230, 100],
+            [700, config.height - 280, config.width - 700],
+            [100, config.height - 330, config.width - 200],
+            [0, config.height - 600, 200]
+        ];
+
+        platforms.forEach((platform) => {
+            this.createPlatform(...platform);
+        });
     }
 
     createPortal(name, x, y, width = 16, height = 16) {
         const portal = this.add.rectangle(x, y, width, height, 0x0000ff);
-        portal.setAlpha(0);
+        // portal.setAlpha(0);
         portal.setOrigin(0, 0);
         this.physics.add.existing(portal);
         portal.body.setImmovable(true);
@@ -47,13 +74,21 @@ class Game extends Phaser.Scene {
     }
 
     createPortals() {
-        this.createPortal('zero', 10, config.height - config.platformHeight - 20);
-        this.createPortal('one', config.width - 16, config.height - 16);
-        this.createPortal('two', 10, config.height - config.platformHeight - config.playerHeight - 100);
-        this.createPortal('three', 150, config.height - config.platformHeight - config.playerHeight - 100);
-        this.createPortal('four', 500, config.height - config.platformHeight - config.playerHeight - 100);
-        this.createPortal('five', 300, config.height - config.platformHeight - config.playerHeight - 100);
-        this.createPortal('six', 10, config.height - config.platformHeight * 3 - config.playerHeight - 150);
+        const portals = [
+            ['zero', 10, config.height - 40],
+            ['one', config.width - 16, config.height - 16],
+            ['two', 10, config.height - 100 - 16],
+            ['three', 150, config.height - 100 - 16],
+            ['four', 500, config.height - 100 - 16],
+            ['five', 300, config.height - 100 - 16],
+            ['six', 10, config.height - 200],
+            ['seven', 0, config.height - 330, 100],
+            ['eight', 10, config.height - 680]
+        ];
+
+        portals.forEach((portal) => {
+            this.createPortal(...portal);
+        });
     }
 
     addPortalMapping(portal1, portal2) {
@@ -81,7 +116,8 @@ class Game extends Phaser.Scene {
             three: 'four',
             four: 'three',
             five: 'six',
-            six: 'zero'
+            six: 'zero',
+            seven: 'eight'
         };
         Object.keys(mapping).forEach((key) => {
             this.addPortalMapping(this.portals[key], this.portals[mapping[key]]);
@@ -89,13 +125,14 @@ class Game extends Phaser.Scene {
     }
 
     setupCamera() {
-        this.cameras.main.setBounds(0, 0, config.width, config.height * 10);
+        this.cameras.main.setBounds(0, -config.height * 10, config.width, config.height * 11);
         this.cameras.main.startFollow(this.player);
     }
 
     create() {
         addFullScreenButton(this);
         this.createPlayer();
+        this.createBoundaries();
         this.cursors = this.input.keyboard.createCursorKeys();
         this.createPlatforms();
         this.createPortals();
